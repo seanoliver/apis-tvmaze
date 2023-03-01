c8b678c610b47e551b7fe9f2f3dd02b3bc91373d
Sean Oliver <882952+seanoliver@users.noreply.github.com>
2023-03-01 12:33:32 -0800

    implemented getShowsByTerm function

--- tvmaze.js
+++ tvmaze.js
@@ -12,27 +12,54 @@ const $searchForm = $("#searchForm");
  *    (if no image URL given by API, put in a default image URL)
  */
 
-async function getShowsByTerm( /* term */) {
+async function getShowsByTerm(searchTerm) {
   // ADD: Remove placeholder & make request to TVMaze search shows API.
+  const request = await axios.get(`http://api.tvmaze.com/search/shows?`,
+                                  { params: { q: searchTerm } });
+
+  console.log('request', request)
+  const showsArray = [];
+
+  for (const show of request.data) {
+    const showId = show.show.id;
+    const showName = show.show.name;
+    const showSummary = show.show.summary;
+
+    const showImage = show.show.image === null
+                        ? `https://picsum.photos/200/300`
+                        : show.show.image.original;
+
+    showsArray.push({
+      id: showId,
+      name: showName,
+      summary: showSummary,
+      image: showImage
+    });
+    console.log('showsArray', showsArray);
+  }
+
+  console.log('request:', request);
+
+
 
   return [
     {
       id: 1767,
       name: "The Bletchley Circle",
       summary:
-        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary 
+        `<p><b>The Bletchley Circle</b> follows the journey of four ordinary
            women with extraordinary skills that helped to end World War II.</p>
-         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their 
-           normal lives, modestly setting aside the part they played in 
-           producing crucial intelligence, which helped the Allies to victory 
+         <p>Set in 1952, Susan, Millie, Lucy and Jean have returned to their
+           normal lives, modestly setting aside the part they played in
+           producing crucial intelligence, which helped the Allies to victory
            and shortened the war. When Susan discovers a hidden code behind an
-           unsolved murder she is met by skepticism from the police. She 
+           unsolved murder she is met by skepticism from the police. She
            quickly realises she can only begin to crack the murders and bring
            the culprit to justice with her former friends.</p>`,
       image:
-          "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
+        "http://static.tvmaze.com/uploads/images/medium_portrait/147/369403.jpg"
     }
-  ]
+  ];
 }
 
 
@@ -43,11 +70,11 @@ function populateShows(shows) {
 
   for (let show of shows) {
     const $show = $(
-        `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
-           <img 
-              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg" 
-              alt="Bletchly Circle San Francisco" 
+           <img
+              src="http://static.tvmaze.com/uploads/images/medium_portrait/160/401704.jpg"
+              alt="Bletchly Circle San Francisco"
               class="w-25 me-3">
            <div class="media-body">
              <h5 class="text-primary">${show.name}</h5>
@@ -56,11 +83,12 @@ function populateShows(shows) {
                Episodes
              </button>
            </div>
-         </div>  
+         </div>
        </div>
       `);
 
-    $showsList.append($show);  }
+    $showsList.append($show);
+  }
 }
 
 
